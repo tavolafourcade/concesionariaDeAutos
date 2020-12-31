906e46afc7a1dfbb5c248412fdf465dd70f9e702
tavolafourcade <tavolafourcade@gmail.com>
2020-12-30 23:13:50 -0500

    Added more funcionalities to the objects

--- concesionaria.js
+++ concesionaria.js
@@ -1,4 +1,26 @@
 /* requerir módulo autos */
 let autos = require('./autos');
 
-console.log(autos)
+
+
+const concesionaria = {
+  autos: autos,
+  buscarAuto: function(patente){
+    for(let i = 0; i < autos.length; i++){
+      if(autos[i].patente == patente){
+        return autos[i];
+      }else{
+        return null
+      }
+    }
+  },
+  venderAuto: function(patente){
+    let autoEncontrado = this.buscarAuto(patente);
+    autoEncontrado ? autoEncontrado.vendido=true:"";
+    return autoEncontrado.vendido;
+  }
+}
+
+// console.log(autos[1].patente)
+// console.log(concesionaria.buscarAuto('APL123'));
+console.log(concesionaria.venderAuto('APL123'))
